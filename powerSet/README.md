80f270b052f76db1003b69501c1fc2dbbe4f5358
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-26 11:45:57 -0500

    powerset

---
+++ powerSet/README.md
@@ -0,0 +1,7 @@
+# Goal
+
+Return an array that contains the power set of a given string. The power set is a set of all the possible subsets, including the empty set.
+
+# status
+
+finished
---
+++ powerSet/index.js
@@ -0,0 +1,30 @@
+function removeDup(string){
+  var output = '';
+  for (var i = 0; i < string.length; i++) {
+    if (output.indexOf(string[i]) === -1) {
+      output += string[i]
+    }
+  }
+  
+  return output;
+}
+
+function sortItem(item){
+  return item.split('').sort().join('');
+}
+
+function powerSet (string) {
+  string = removeDup(string);
+  var search = function(active, rest, output) {
+    if (!active && !rest)
+      return;
+    if (!rest) {
+      output.push(sortItem(active));
+    } else {
+      search(active + rest[0], rest.slice(1), output);
+      search(active, rest.slice(1), output);
+    }
+    return output.sort();
+  }
+  return search("", string, ['']);
+} 

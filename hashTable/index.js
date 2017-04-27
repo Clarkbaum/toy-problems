0f099ecfce313013106fcdcfd8480d6fc530251a
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-04-27 15:15:41 -0500

    hashtable

---
+++ hashTable/index.js
@@ -0,0 +1,29 @@
+  var makeHashTable = function(){
+    var table = {};
+    var storage = [];
+    var storageLimit = 1000;
+
+    table.insert = function(key, value){
+      var index = getIndexBelowMaxForKey(key, storageLimit);
+      storage[index] = storage[index] || [];
+      //here
+    };
+
+    table.retrieve = function(key){
+      //here
+    };
+
+    table.remove = function(key){
+      //here
+    }
+    return table;  
+  };  
+ var getIndexBelowMaxForKey = function(str, max){
+   var hash = 0;
+   for (var i = 0; i < str.length; i++) {
+     hash = (hash<<5) + hash + str.charCodeAt(i);
+     hash = hash & hash; // Convert to 32bit integer
+     hash = Math.abs(hash);
+   }
+   return hash % max;
+ };

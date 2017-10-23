2411bcfad63c255c133cc63cdc3ac73ba498d25b
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-10-23 18:53:27 -0500

    added error handleing and neg num

--- stockPricesYesterday/index.js
+++ stockPricesYesterday/index.js
@@ -1,9 +1,13 @@
 //Note this was tested on codepen were its connected to a simple html doc with a single p tag with "output" id
 
-var stockPricesYesterday = [10, 9, 8, 7, 6, 5,4,3,2,1];
+var stockPricesYesterday = [10, 9, 13, 44, 9, 1,2,18,2,1];
 var differenceArr = [];
 
 function getMaxProfit () {
+  if (stockPricesYesterday.length < 2) {
+    throw new Error("Getting profit requires at least 2 prices");
+  }
+  
   for (var i = 0; i < stockPricesYesterday.length - 1; i++) {
     var difference = largestToRight(stockPricesYesterday.slice(i + 1)) - stockPricesYesterday[i];
     differenceArr.push(difference);
@@ -12,7 +16,7 @@ function getMaxProfit () {
   var output =  differenceArr.reduce(function(a,b) {
     return Math.max(a, b)
   })
-  
+
   if(output < 0 ) {output = 0}
   
   document.getElementById("output").innerHTML = output;
@@ -25,3 +29,4 @@ function largestToRight (arr) {
   });
   return max;
 }
+

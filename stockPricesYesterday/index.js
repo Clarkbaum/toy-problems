247e46e11d7b72980a273a750508dff6ecc6e41a
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-10-23 18:38:08 -0500

    handle if price keeps going down

--- stockPricesYesterday/index.js
+++ stockPricesYesterday/index.js
@@ -1,8 +1,9 @@
-var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+//Note this was tested on codepen were its connected to a simple html doc with a single p tag with "output" id
+
+var stockPricesYesterday = [10, 9, 8, 7, 6, 5,4,3,2,1];
 var differenceArr = [];
 
 function getMaxProfit () {
-  document.getElementById("output").innerHTML = Math.PI;
   for (var i = 0; i < stockPricesYesterday.length - 1; i++) {
     var difference = largestToRight(stockPricesYesterday.slice(i + 1)) - stockPricesYesterday[i];
     differenceArr.push(difference);
@@ -11,6 +12,8 @@ function getMaxProfit () {
   var output =  differenceArr.reduce(function(a,b) {
     return Math.max(a, b)
   })
+  
+  if(output < 0 ) {output = 0}
   
   document.getElementById("output").innerHTML = output;
   

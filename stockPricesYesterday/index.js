98be8775b5e7966224950c435c2949a0072356a5
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-10-23 16:13:38 -0500

    working toy problem

--- stockPricesYesterday/index.js
+++ stockPricesYesterday/index.js
@@ -1,1 +1,24 @@
-index.js
+var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+var differenceArr = [];
+
+function getMaxProfit () {
+  document.getElementById("output").innerHTML = Math.PI;
+  for (var i = 0; i < stockPricesYesterday.length - 1; i++) {
+    var difference = largestToRight(stockPricesYesterday.slice(i + 1)) - stockPricesYesterday[i];
+    differenceArr.push(difference);
+  }
+  
+  var output =  differenceArr.reduce(function(a,b) {
+    return Math.max(a, b)
+  })
+  
+  document.getElementById("output").innerHTML = output;
+  
+}
+
+function largestToRight (arr) {
+  var max = arr.reduce(function(a, b) {
+    return Math.max(a, b)
+  });
+  return max;
+}

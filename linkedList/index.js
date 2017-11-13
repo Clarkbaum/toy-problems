d5bd2b7abfb3477ff73d9aeca9a510bccf36bff3
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-11-12 18:13:25 -0600

    spacing

--- linkedList/index.js
+++ linkedList/index.js
@@ -14,19 +14,19 @@ LinkedList.prototype.addToTail = function (value) {
   this.tail.next = node;
   this.tail = node;
   
-  if(this.head.value === undefined) this.head = node;
+  if (this.head.value === undefined) this.head = node;
 }
 
 LinkedList.prototype.removeHead = function() {
   this.head = this.head.next
-  if(this.head === null) this.tail = null
+  if (this.head === null) this.tail = null
 }
 
 LinkedList.prototype.contains = function (search) {
   var curr = this.head
   var output = false;
-  while(curr !== null){
-    if(curr.value === search){
+  while (curr !== null) {
+    if (curr.value === search) {
       output = true;
       return output;
     } else {

7c9c74abb6496b36af11fd4fab9becbc418d4633
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2019-07-30 13:26:48 -0500

    binarysearch practice

---
+++ BinarySearchArray/index2.js
@@ -0,0 +1,22 @@
+//more practice for binary search 2019
+
+function binarySearch(origArray, target) {
+    let search = function(array, target) {
+        let middle = array[Math.floor(array.length / 2)];
+        if (target === middle) {
+            return origArray.indexOf(middle);
+        } else if ( target >= middle) {
+            return search(array.slice(array.indexOf(middle) + 1), target);
+        } else if ( target <= middle) {
+            return search(array.slice(0, array.indexOf(middle)), target)
+        } else {
+            return -1;
+        }
+
+    };
+
+    return search(origArray, target);
+
+}
+
+console.log(binarySearch([1,3,5,6,7,9,12,14,25,26], 25));
